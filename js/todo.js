a3e940b5a86ab0662812189e8f905be36571a3cc
Scott Baker <scott@perturb.org>
2018-03-06 08:13:02 -0800

    Toggle the add note box off if you click again

--- js/todo.js
+++ js/todo.js
@@ -13,10 +13,11 @@ function add_note(selector) {
 
 	// If this item already has a note box open don't add a second
 	var has_note = $(".adding_note", elem).length > 0;
-	if (has_note || !id) { return false; }
 
 	// Only one note box open at at time, remove all the other ones
 	$(".add_note_wrapper").remove();
+
+	if (has_note || !id) { return false; }
 
 	var note = $("<div class=\"add_note_wrapper\"><form class=\"adding_note\" method=\"get\" action=\"index.php\"> <input class=\"add_note\" placeholder=\"Add notes...\" type=\"text\" id=\"ta_" + id + "\" name=\"note\" /> <input type=\"hidden\" name=\"note_id\" value=\"" + id + "\"> <input type=\"hidden\" name=\"action\" value=\"add_note\"></form></div>");
 
