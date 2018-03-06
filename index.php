78c932926febead79b003ed3d8e631254979b28f
Scott Baker <scott@perturb.org>
2018-03-06 10:31:12 -0800

    Add some debug information

--- index.php
+++ index.php
@@ -1,5 +1,8 @@
 <?php
 
+$debug = $_GET['debug'] ?? "";
+$start = microtime(1);
+
 require('include/todo.class.php');
 require('include/xhtml.class.php');
 
@@ -15,9 +18,11 @@ $out          = $todo->show_todo_list();
 $bookmarklet  = $todo->get_bookmarklet();
 $out         .= "<div class=\"footer\"><a href=\"javascript: $bookmarklet\">Bookmarklet</a></div>";
 
-// Add the sql output if it's requested
-if (!empty($_GET['debug'])) {
-	$out .= "<br />" . $todo->dbq->query_summary();
+// Output some debug info if requested
+if ($debug) {
+	$total = sprintf("%0.3f", microtime(1) - $start);
+	$out .= "<p>Rendered in $total seconds</p>";
+	$out .= $todo->dbq->query_summary();
 }
 
 print $xhtml->output($out);
