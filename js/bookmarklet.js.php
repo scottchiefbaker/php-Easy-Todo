25274562e100d440c2e6a88421b9ab08a356003a
Scott Baker <scott@perturb.org>
2018-05-08 14:19:33 -0700

    Modernize the too bookmarklet

--- js/bookmarklet.js.php
+++ js/bookmarklet.js.php
@@ -7,10 +7,23 @@ else { $http = "http://"; }
 
 $post_location = $http . $_SERVER['SERVER_NAME'] . dirname($_SERVER['REQUEST_URI']) . "/../";
 
+$html = "
+<div style=\"z-index: 255; border: 1px solid gray; background-color: #f7ffff; text-align: center; position: absolute; top: 15px; left: 15px; border-radius: 5px; width: calc(100% - 30px); \" id=\"add_todo\">
+    <div style=\"padding: 30px 20px;\">
+        <form action=\"$post_location\" method=\"get\">
+            <input name=\"todo_desc\" style=\"font-size: 150%; width: 100%; height: 2em; border: 0; outline: 1px solid #e3e3e3; padding: 0px 10px;\" id=\"text_area\" placeholder=\"Pick up dry cleaning\" ></input>
+            <input type=\"hidden\" name=\"action\" value=\"add_todo\" />
+        </form>
+    </div>
+</div>";
+
+$html = preg_replace("/\"/","\\\"",$html);
+$html = preg_replace("/\n/","",$html);
+
 ?>
 var eid = document.getElementsByTagName('body');
 
-eid[0].innerHTML = eid[0].innerHTML + "<div style=\"z-index: 255; border: 1px solid black; background-color: white; text-align: center; position: absolute; top: 5px; left: 5px; width: 98%; \" id=\"add_todo\"> <div style=\"padding: 15px; \"><a onclick=\"javascript: var eid = document.getElementById('add_todo'); eid.innerHTML = ''; eid.style.display = 'none'; return false; \" href=\"/\">Hide</a> <form action=\"<?php print $post_location; ?>\" method=\"get\"> <textarea name=\"todo_desc\" style=\"width: 90%; height: 5em; \" id=\"text_area\"></textarea> <input type=\"hidden\" name=\"action\" value=\"add_todo\" /> <br /> <input type=\"submit\" value=\"Add item\" /> </form> </div> </div>";
+eid[0].innerHTML = eid[0].innerHTML + "<?php print $html; ?>";
 
 location.href = "#";
 document.getElementById('text_area').focus();
